bb6c1749c27b8f41ed92745cefbe6d281b4e4cfe
JoelitoCS <canosanchezjoel@fpllefia.com>
2026-04-21 12:15:37 +0200

    cambios para conectar a base de datos

--- index.js
+++ index.js
@@ -1,30 +1,26 @@
 import cors from 'cors';
 import express from 'express';
+import mongoose from 'mongoose';
 import routerEquipos from './rutas/equipos.js';
 import dotenv from 'dotenv';
 dotenv.config();
+
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+// Conexión a MongoDB
+mongoose.connect(process.env.MONGODB_URI)
+    .then(() => console.log('Conectado a MongoDB'))
+    .catch(err => console.error('Error al conectar a MongoDB:', err));
+
 app.use(cors());
-//middleware para rutas
 app.use(express.json());
 app.use("/api/equipos", routerEquipos);
 
 app.get('/', (req, res) => {
     res.send('API de la remontada ' + process.env.EJEMPLO);
-    console.log('Se ha recibido una solicitud GET en la ruta /');
 });
 
-// app.get("api/equipos", (req,res) => {
-//     console.log('Se ha recibido una solicitud GET en la ruta /api/equipos');
-//     res.json ({
-//         mensaje: 'Lista de equipos',
-//         equipos: []
-//     })
-// })
-
-app.listen(PORT,() => {
+app.listen(PORT, () => {
     console.log('Se esta ejecutando el puerto= ' + PORT);
-    
 });
